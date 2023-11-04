bd840737b7044ed06a9c51556c84fce332f7831f
KyaraZhan <shangrilas@live.com>
2023-11-05 03:49:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,9 @@
 #Clone SSRPlus source code
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
-#Clone OpenAppFilter source code
-#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
